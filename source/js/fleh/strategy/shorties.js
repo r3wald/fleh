27e57625f68b2e2ae8ab87303a12e328d6bf1c29
Robert Ewald <r3wald@gmail.com>
2010-10-05 12:15:53 +0200

    strategy 'shorties' implemented.

--- source/js/fleh/strategy/shorties.js
+++ source/js/fleh/strategy/shorties.js
@@ -8,15 +8,27 @@ Fleh.Strategy.Shorties = new Class({
 	
 	description: 'Es werden nur einstündige Jobs angenommen.',
 	
-	// @todo
+	/**
+	 * just return jobs with one hour
+	 */
 	select: function(jobs) {
+		var result = null;
 		if (!jobs || jobs.length<1) {
 			console.error('no jobs given!');
 		}
-		return jobs[0];
+		// console.log(jobs.length,jobs);
+		jobs = jobs.filter(function(job) {
+			var hours = job.getElement('.facts .hours').get('text');
+			return (hours==1);
+		});
+		if (jobs.length>0) {
+			result = jobs[0];
+		}
+		// console.log(jobs.length,jobs);
+		return result;
 	}
 
 });
 
-//Fleh.Strategy.instances.push(new Fleh.Strategy.Shorties());
+Fleh.Strategy.instances.push(new Fleh.Strategy.Shorties());
 
