02f7bc434e676781271731e7f8320a5d1a35272d
Robert Ewald <r.ewald@esvmedien.de>
2010-12-23 14:11:12 +0100

    Fleh.Tools.clickButton() added.

--- source/js/fleh/tools.js
+++ source/js/fleh/tools.js
@@ -1,36 +1,42 @@
 
 Fleh.Tools = {
 
-	load: function(url){
-		window.location.href=url;
-	},
+		clickButton: function(button){
+			var event = document.createEvent( "MouseEvents" );
+			event.initMouseEvent("click", true, true, window, 1, 1, 1, 1, 1, false, false, false, false, 0, button);
+			button.dispatchEvent( event );		
+		},
 
-	reload: function(){
-		this.load(window.location.href);
-	},
+		load: function(url){
+			window.location.href=url;
+		},
 
-	reloadAfter: function(seconds){
-		setTimeout(
-			function(){
-				this.reload();
-			}.bind(this),
-			1000 * seconds
-		);
-	},
+		reload: function(){
+			this.load(window.location.href);
+		},
 
-	formatTime: function(minutes){
-		var result, hours;
-		result = minutes.toString() + 'min';
-		if (minutes > 60) {
-			hours = Math.ceil(minutes / 60);
-			minutes = minutes % 60;
-			result = hours + ':' + (minutes < 10 ? '0' : '') + minutes + 'h';
+		reloadAfter: function(seconds){
+			setTimeout(
+					function(){
+						this.reload();
+					}.bind(this),
+					1000 * seconds
+			);
+		},
+
+		formatTime: function(minutes){
+			var result, hours;
+			result = minutes.toString() + 'min';
+			if (minutes > 60) {
+				hours = Math.ceil(minutes / 60);
+				minutes = minutes % 60;
+				result = hours + ':' + (minutes < 10 ? '0' : '') + minutes + 'h';
+			}
+			return result;
+		},
+
+		parseNumber: function(value) {
+			return parseInt(value.replace('.','').replace('%',''));
 		}
-		return result;
-	},
-	
-	parseNumber: function(value) {
-		return parseInt(value.replace('.','').replace('%',''));
-	}
 
 };
